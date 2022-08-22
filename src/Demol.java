4f0e4fbbc35751fa34a1d1ca508df1034aaad5af
lipan123456 <1109180397@qq.com>
2022-08-22 18:09:40 +0800

    demol

---
+++ src/Demol.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Demol {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(1);
+        System.out.println(list);
+        System.out.println("list");
+    }
+
+}
